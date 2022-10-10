cb717771ce72bef7ab5fc85b67c7916c38883d54
Julius Härtl <jus@bitgrid.net>
2022-10-10 11:20:43 +0200

    Initial commit
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @copyright Copyright (c) 2022 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\NmcOfficeReport\AppInfo;
+
+use OCA\NmcOfficeReport\Listener\DocumentOpenedListener;
+use OCA\Richdocuments\Events\DocumentOpenedEvent;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends \OCP\AppFramework\App implements \OCP\AppFramework\Bootstrap\IBootstrap {
+
+	public const APP_ID = 'nmc_office_report';
+	public function __construct(array $urlParams = []) {
+		parent::__construct(self::APP_ID, $urlParams);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function register(IRegistrationContext $context): void {
+		$context->registerEventListener(DocumentOpenedEvent::class, DocumentOpenedListener::class);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function boot(IBootContext $context): void {
+	}
+}
---
+++ lib/Listener/DocumentOpenedListener.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @copyright Copyright (c) 2022 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\NmcOfficeReport\Listener;
+
+use OCA\NmcOfficeReport\UsageLogger;
+use OCA\Richdocuments\Events\DocumentOpenedEvent;
+use OCP\EventDispatcher\Event;
+
+class DocumentOpenedListener implements \OCP\EventDispatcher\IEventListener {
+
+	private UsageLogger $usageLogger;
+
+	public function __construct(UsageLogger $usageLogger) {
+		$this->usageLogger = $usageLogger;
+	}
+
+
+	public function handle(Event $event): void {
+		if (!$event instanceof DocumentOpenedEvent) {
+			return;
+		}
+
+		$this->usageLogger->error($event->getUserId() . ' opened the file ' . $event->getNode()->getId(), [
+			'userId' => $event->getUserId(),
+		]);
+	}
+}
